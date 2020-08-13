7bd19fd800152c249a4fa654974e23bd6809874f
David Tounn <7319dtounn@gmail.com>
2020-08-13 09:54:15 -0700

    new files

---
+++ FirstProgram/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("My favorite color is green");
+            Console.ReadLine();
+        }
+    }
+}
