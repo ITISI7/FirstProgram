3f80239888fd6c7c89d582cc4837626ee7e40d14
David Tounn <7319dtounn@gmail.com>
2020-08-13 11:33:40 -0700

    New Files

---
+++ VariableOperatorsExpressions/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariableOperatorsExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I want to work for Umbrella Corp.");
+            Console.ReadLine();
+        }
+    }
+}
