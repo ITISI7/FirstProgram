dc8993711d34f37423cda85fa0da1eff31f9a997
David Tounn <7319dtounn@gmail.com>
2020-08-14 11:28:49 -0700

    New

---
+++ FunctionTutorial/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace FunctionTutorial
+{
+    class Program
+    {
+        //static int playerHealth = 100;
+        //static int monsterDamage = 30;
+        //static void MonsterEncounter()
+        //{
+        //    Console.WriteLine("A monster appears");
+        //    Console.ReadKey();
+        //}
+        //static void Start()
+        //{
+        //    Console.WriteLine("The game had began");
+        //    Console.ReadKey();
+        //    MonsterEncounter();
+        //    MonsterAttacks();
+        //    MonsterAttacks();
+        //    MonsterAttacks();
+
+        //}
+        //static void MonsterAttacks()
+        //{
+        //    playerHealth -= monsterDamage;
+        //    Console.WriteLine("The monster attacks you for " + monsterDamage +
+        //        " points of damage, you have " + playerHealth + " health left");
+        //    Console.ReadKey();
+        //}
+        //static void Main(string[] args)
+        //{
+        //    Start();
+        //}
+
+        static int numberOne = 10;
+        static int numberTwo = 200;
+        static void Start()
+        {
+            int biggestNumber = ReturnBiggestNumber(numberOne, numberTwo);
+            Console.WriteLine("Of " + numberOne + " and " + numberTwo + " the biggest number is " +
+                biggestNumber);
+           
+            Console.ReadKey();
+        }
+        static int ReturnBiggestNumber(int first, int second)
+        {
+            if (first > second)
+            {
+                return first;
+            }
+            else
+            {
+                return second;
+            }
+        }
+        static void Main()
+        {
+            Start();
+        }
+    }
+}
+
---
+++ functionExercise/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks.Sources;
+
+namespace functionExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            game cl = new game();
+            cl.Start();
+        }
+
+      
+    }
+    public class game
+    {
+        static int score = 100;
+        private static int result;
+
+        public void Start()
+        {
+            int result = AddToScore(score);
+            Console.WriteLine("Your score is " + score);
+            Console.WriteLine("Your score increase it is now " + result);
+            Console.ReadKey();
+        }
+        public static int AddToScore(int add)
+        {
+            result = score + add;
+            return result;
+        }
+    }
+}
