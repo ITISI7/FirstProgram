2f989b7abe17f4c395e44ce281869dc8778e7725
David Tounn <7319dtounn@gmail.com>
2020-08-13 18:00:44 -0700

    New files

---
+++ conditionalStatements/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace conditionalStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ looptutorial/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace looptutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // While loop
+            //int count = 0;
+            //while(count < 3)
+            //{
+            //    count++;
+            //    Console.WriteLine("The loop has run " + count + " times.");
+            //}
+            for(int count = 0; count < 3; count++)
+            {
+                Console.WriteLine("The loop has run " + count + " times.");
+                // how to break
+                //if(count == 2)
+                //{
+                //    break;
+                //}
+            }
+            Console.WriteLine("The loop is finshed");
+            Console.ReadKey();
+
+        }
+    }
+}
